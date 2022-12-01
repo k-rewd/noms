bc1bd42aad04357de3c4a538fe16bde789d7192b
andrew <a.kimswk@gmail.com>
2022-12-01 05:26:08 -0800

    it worked!

--- react-app/src/components/Notes/Notes.js
+++ react-app/src/components/Notes/Notes.js
@@ -24,15 +24,17 @@ const Notes = () => {
 
   if (!notes) return null
   else return (
-    notesArr.map(note => (
-      <div id='note-container'>
-        <div id='note-user-info'>
-        <div id='note-username'>{note.user?.username}</div>
-        <div id='note-time-info'>{note.updated_at ? moment(note.updated_at).fromNow() : moment(note.created_at).fromNow()}</div>
+    <div id='note-order-maybe'>
+      {notesArr.map(note => (
+        <div id='note-container'>
+          <div id='note-user-info'>
+            <div id='note-username'>{note.user?.username}</div>
+            <div id='note-time-info'>{note.updated_at ? moment(note.updated_at).fromNow() : moment(note.created_at).fromNow()}</div>
+          </div>
+          <div id='note-body'>{note.note_body}</div>
         </div>
-        <div id='note-body'>{note.note_body}</div>
-      </div>
-    ))
+      ))
+      }</div>
   )
 }
 
